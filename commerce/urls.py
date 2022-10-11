97c3b8d1db77676e6398a9269c88c03a9cfc398e
igruk <igruk_md@ukr.net>
2022-10-11 23:12:42 +0300

    fix cracked picture

--- commerce/urls.py
+++ commerce/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from commerce.commerce import settings
+from commerce import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
